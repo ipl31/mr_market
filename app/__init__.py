358eccbe97dc12fc4e1b1502e8a5a4f4e8030b46
Ken Caruso <ken@ipl31.net>
2020-11-18 20:28:21 +0000

    add simple hello route in index

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,10 @@ logging.basicConfig(level=logging.DEBUG)
 slack_events_adapter = SlackEventAdapter(os.environ.get("SLACK_EVENTS_TOKEN"), "/slack/events", app)
 slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN"))
 
+@app.route("/")
+def hello():
+    return "Hello"
+
 @slack_events_adapter.on("message")
 def handle_message(data):
     """ Handle slack message events """
@@ -18,4 +22,5 @@ def handle_message(data):
     return None
 
 if __name__ == '__main__':
-    app.run()
+    #TODO: Turn off debug.
+    app.run(debug=True)
