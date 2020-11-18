c3b94bea8101a7e2d5d2c58da84d6dcbb17447a1
Ken Caruso <ken@ipl31.net>
2020-11-18 21:22:29 +0000

    add basic test

---
+++ test_mr_market.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app
+
+
+
+def test_index():
+    client = app.test_client()
+    response = client.get('/', content_type='html/text')
+    assert response.status_code == 200
+
+def test_404():
+    client = app.test_client()
+    response = client.get('/asdasdasda', content_type='html/text')
+    assert response.status_code == 404 
+
