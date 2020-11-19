f1d3c46b5fe3ab9447078c2d16811a5057381c1c
Ken Caruso <ken@ipl31.net>
2020-11-19 06:44:46 +0000

    don't use logger from flask app

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from slackeventsapi import SlackEventAdapter
 from slack import WebClient
 
 logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 # TODO: Tokens should be in app.config
 events_token = os.environ.get("SLACK_EVENTS_TOKEN")
 client_token = os.environ.get("SLACK_TOKEN")
@@ -42,7 +43,7 @@ def hello():
 @slack_events_adapter.on("message")
 def handle_message(data):
     """ Handle slack message events """
-    app.logger.debug("Handling slack message event.")
+    logger.debug("Handling slack message event.")
     bot_id = slack_client.api_call("auth.test")['user_id']
     event = data.get("event", {})
     user_id = event.get("user")
@@ -50,10 +51,10 @@ def handle_message(data):
     channel_id = event.get("channel")
 
     if bot_id == user_id:
-        app.logger.debug("Message is from myself, skipping.")
+        logger.debug("Message is from myself, skipping.")
         return
     if bot_id not in text:
-        app.logger.debug("Message is not addressed to me. Skipping")
+        logger.debug("Message is not addressed to me. Skipping")
         return
 
     if "hello" in text.lower():
@@ -69,7 +70,7 @@ def handle_message(data):
             message = f"Symbol: {symbol}  Price: {price}"
             post_message(channel_id, message)
         except Exception:
-            app.logger.execption("Error getting stock price")
+            logger.execption("Error getting stock price")
             post_message(channel_id, f"I encountered an error for {symbol}")
             return
         return
