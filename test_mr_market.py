6c4412fa53fcb432b74f3eff5376eb22adf3abbf
Ken Caruso <ken@ipl31.net>
2020-11-20 03:36:05 +0000

    refactor test_MisterMarketBot to different functions/tests for each test

--- test_mr_market.py
+++ test_mr_market.py
@@ -36,20 +36,41 @@ class barSkill():
         return [param, "bar"]
 
 
-def test_MisterMarketBot():
-    skills = {"foo": fooSkill, "bar": barSkill}
-    bot_id = 'abc123'
-    bot = MisterMarketBot(skills, bot_id)
+skills = {"foo": fooSkill, "bar": barSkill}
+bot_id = 'abc123'
+bot = MisterMarketBot(skills, bot_id)
+
+
+def test_MisterMarketBot_is_message_command():
     assert bot._is_message_command("bar") is True
     assert bot._is_message_command("blah") is False
+
+
+def test_MisterMarketBot_is_payload_from_me():
     assert bot._is_payload_from_me({"user": "123"}) is False
     assert bot._is_payload_from_me({"user": bot_id}) is True
+
+
+def test_MisterMarketBot_get_skills():
     assert "foo" in bot._get_skills()
     assert "bar" in bot._get_skills()
+
+
+def test_MisterMarketBot_get_skills_commands():
     assert "get_foo" in bot._get_skill_commands("foo")
     assert "get_bar" in bot._get_skill_commands("bar")
     assert "get_bar_with_param" in bot._get_skill_commands("bar")
     assert "get_foo" not in bot._get_skill_commands("bar")
+
+
+def test_MisterMarketBot_parse_skill_command():
+    skill, command, args = bot._parse_skill_command("bah get_bah param1 param2")
+    assert skill == "bah" 
+    assert command == "get_bah" 
+    assert args == ["param1", "param2"]
+
+
+def test_MisterMarketBot_run_skill_command():
     result = bot._run_skill_command("foo", "get_foo")
     assert result == "foo"
     result = bot._run_skill_command("bar", "get_bar_with_param",  "baz")
