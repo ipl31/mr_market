d58493c93567900dd798b71d6e287571603e4ef3
Ken Caruso <ken@ipl31.net>
2020-12-04 06:37:16 +0000

    add GCUSD

--- src/mister_market/stock_commands.py
+++ src/mister_market/stock_commands.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from functools import lru_cache
 from iexfinance import altdata, refdata
 from iexfinance.stocks import Stock
@@ -5,10 +7,28 @@ from iexfinance.utils.exceptions import IEXQueryError
 from slackblocks import HeaderBlock, DividerBlock, SectionBlock
 from .plugin_base import PluginBase
 
+GOLD_ALIASES = ["GCUSD", "XAUUSD", "XAU"]
+GOLD_COMM_SYMBOL = "GCUSD"
+
 
 @lru_cache(maxsize=1)
 def get_iex_symbol_universe():
     return refdata.get_symbols()
+
+
+def get_fmp_quote(symbol):
+    key = os.environ.get("FMP_API_KEY")
+    url = "https://financialmodelingprep.com/api/v3/quote"
+    response = requests.get(f"{url}/{symbol}?apikey={key}")
+    for dictionary in response.json():
+        if dictionary['symbol'] == GOLD_COMM_SYMBOL:
+            return dictionary
+    raise Exception("Quote not found in response")
+
+
+def get_gold_price():
+    quote = get_fmp_quote(GOLD_COMM_SYMBOL)
+    return quote['price']
 
 
 def is_symbol_in_iex_universe(symbol):
@@ -32,6 +52,48 @@ class QuoteCommand(PluginBase):
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def _build_gold_quote_msg_block(quote):
+        blocks = []
+
+        symbol = quote.get("symbol")
+        name = quote.get("name")
+        price = commaify(quote.get("price"))
+        high52 = commaify(quote.get("yearHigh"))
+        low52 = commaify(quote.get("yearLow"))
+        avg50 = commaify(quote.get("priceAvg50"))
+        avg200 = commaify(quote.get("priceAvg200"))
+        volume = commaify(quote.get("volume"))
+        avg_volume = commaify(quote.get("avgVolume"))
+        prev_close = commaify(quote.get("previousClose"))
+
+        blocks.append(
+                HeaderBlock(
+                    text=f"{symbol} - {name}"))
+        blocks.append(DividerBlock)
+
+        blocks.append(
+                SectionBlock(
+                    text=f"*Price:* {price} *--* *Prev. Close:* {prev_close}"))
+        blocks.append(DividerBlock)
+
+        blocks.append(
+                SectionBlock(
+                    text=(f"*Volume:* {volume} *--* "
+                          f"*Avg Volume:* {avg_volume}")))
+        blocks.append(DividerBlock)
+
+        blocks.append(
+                SectionBlock(
+                    text=((f"*52 High:* {high52} *--* "
+                           f"*52w Low:* {low52} *--* "))))
+        blocks.append(DividerBlock)
+
+        blocks.append(
+            SectionBlock(
+                text=f"*50 MA:* {avg50} *--* *200 MA:* {avg200}"))
+        blocks.append(DividerBlock)
 
     @staticmethod
     def _build_quote_msg_block(quote):
@@ -69,6 +131,11 @@ class QuoteCommand(PluginBase):
 
         return blocks
 
+    def _get_gold_quote_blocks(self, symbol):
+        quote = get_fmp_quote(symbol)
+        blocks = self._build_gold_quote_msg_block(quote)
+        return blocks
+
     def _get_stock_quote_blocks(self, symbol):
         quote = Stock(symbol).get_quote()
         blocks = self._build_quote_msg_block(quote)
@@ -78,6 +145,10 @@ class QuoteCommand(PluginBase):
         args = list(args)
         symbol = args.pop(0)
         symbol_err = SectionBlock(text=f"Symbol `{symbol}` not found")
+
+        if symbol in GOLD_ALIASES:
+            blocks = self._get_gold_quote_blocks(GOLD_COMM_SYMBOL)
+            return blocks
 
         if is_symbol_in_iex_universe(symbol):
             blocks = self._get_stock_quote_blocks(symbol)
@@ -111,6 +182,11 @@ class PriceCommand(PluginBase):
         args = list(args)
         symbol = args.pop(0)
         symbol_err = f"Symbol `{symbol}` not found"
+        if symbol in GOLD_ALIASES:
+            symbol = GOLD_COMM_SYMBOL
+            price = get_gold_price()
+            return SectionBlock(text=f"`{symbol}` `{price}`")
+
         if is_symbol_in_iex_universe(symbol):
             price = self._get_stock_price(symbol)
             return SectionBlock(text=f"`{symbol}` `{price}`")
