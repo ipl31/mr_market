f853a22abb9eae1953ed1578318033b96e37d400
Kenneth Caruso <ken@ipl31.net>
2021-02-01 00:09:56 -0800

    remove dead code

--- src/mister_market/helpers.py
+++ src/mister_market/helpers.py
@@ -5,18 +5,6 @@ from functools import lru_cache
 from iexfinance import refdata
 
 
-def get_public_methods(object):
-    methods = []
-    for method_name in dir(object):
-        if method_name.startswith("__"):
-            continue
-        if method_name.startswith("_"):
-            continue
-        if callable(getattr(object, method_name)):
-            methods.append(str(method_name))
-    return methods
-
-
 def commaify(data):
     if data is None:
         return data
